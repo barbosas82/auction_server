f46a4ff432cd0084a90b12da24c30cca3a982490
barbosas82 <pedrobarbosa@pb.com.pt>
2017-01-30 22:54:10 +0000

    master

--- controllers/user.js
+++ controllers/user.js
@@ -34,33 +34,13 @@ exports.listOne = function(req, res){
 exports.Auth = function(req, res){
   var username = req.body.username;
 
-   User.find({"username": username}, function (err, user){
-
-     //res.send("Username: " + username);
-
-     if(err) throw err;
-
-     if (!user) {
-       res.json({ success: false, message: 'Authentication failed. User not found. ' + username});
-     } else if (user) {
-
-         // check if password matches
-         if (user.password != req.body.password) {
-           res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-         } else {
-           // if user is found and password is right
-           // create a token
-           var token = jwt.sign(user, "passwordlixada123", {expiresInMinutes: 1000});
-
-           // return the information including token as JSON
-           res.json({
-             success: true,
-             message: 'Enjoy your token!',
-             token: token
-           });
-         }
-     }
-   });
+  User.findOne({"username": username}, function(err, exists){
+      if (exists){
+        return res.send("User " + username + " already exists.");
+      }else{
+        return res.send("COCO");
+      }
+  });
 
 
 };
