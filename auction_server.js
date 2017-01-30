e2a788a594f34b696c15696c404f15a84d20cb72
barbosas82 <pedrobarbosa@pb.com.pt>
2017-01-30 15:19:49 +0000

    master

---
+++ auction_server.js
@@ -0,0 +1,37 @@
+// =====================================
+// get the packages we need ============
+// =====================================
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
+require('./routes')(app); //get our routes
+
+
+
+// =======================
+// configuration =========
+// =======================
+var db = mongoose.connect(config.mongoose.uri);
+autoIncrement.initialize(db);
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
---
+++ routes.js
@@ -0,0 +1,10 @@
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+// API ROUTES -------------------
+// we'll get to these in a second
