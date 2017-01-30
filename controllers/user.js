9c7e4d86ca7aab55514f07393a287297d21e087d
barbosas82 <pedrobarbosa@pb.com.pt>
2017-01-30 22:56:28 +0000

    master

--- controllers/user.js
+++ controllers/user.js
@@ -34,7 +34,7 @@ exports.listOne = function(req, res){
 exports.Auth = function(req, res){
   var username = req.body.username;
 
-  User.findOne({"username": username}, function(err, exists){
+  User.find({"username": username}, function(err, exists){
       if (exists){
         return res.send("User " + username + " already exists.");
       }else{
