dbf31850e4ed6616f1e8de5d94e09be388a50c03
barbosas82 <pedrobarbosa@pb.com.pt>
2017-01-30 15:29:43 +0000

    master

--- auction_server.js
+++ auction_server.js
@@ -6,7 +6,7 @@ var app           = express();
 var bodyParser    = require('body-parser');
 var morgan        = require('morgan');
 var mongoose      = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment'),
+var autoIncrement = require('mongoose-auto-increment');
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
