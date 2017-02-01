4642f36832ff1202d2d55f3a61a8adb6430d1e60
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-01 18:11:02 +0000

    master

--- routes.js
+++ routes.js
@@ -8,15 +8,20 @@ module.exports = function(app){
 
   var config = require('./config'); // get our config file
 
-// API ROUTES -------------------
+  /**********************************************
+  ******    DEFAULT MESSAGE                 *****
+  **********************************************/
+  apiRoutes.get('/', function(req, res) {
+       res.json({ message: 'Welcome to the coolest API on earth!' });
+  });
 
-  //Admin method to add user, list users
+  /**********************************************
+  ******        USER METHODS                *****
+  **********************************************/
   var users = require('./controllers/user');
   apiRoutes.post('/useradd', users.add);
   apiRoutes.get('/listusers', users.listAll);
   apiRoutes.get('/listuser/:username', users.listOne);
-  app.post('/authenticate', users.Auth); //Get New Token
-
 
   /**********************************************
   ******    METHODS FOR WANTLIST / ARTIST   *****
@@ -31,11 +36,36 @@ module.exports = function(app){
    /**********************************************
    ******    METHODS FOR TOKENS              *****
    **********************************************/
+   app.post('/authenticate', users.Auth); //Get New Token
 
+   // route middleware to verify a token
+  apiRoutes.use(function(req, res, next) {
 
-   apiRoutes.get('/', function(req, res) {
-        res.json({ message: 'Welcome to the coolest API on earth!' });
-   });
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('secret'), function(err, decoded) {
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+          success: false,
+          message: 'No token provided.'
+      });      
+    }
+  });
+
    app.use('/api', apiRoutes);
 
 
