79e4837c028dca0dc33f87f43f9e8fdf94871bcf
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-01 22:30:55 +0000

    master

--- auction_server.js
+++ auction_server.js
@@ -18,8 +18,8 @@ var config = require('./config'); // get our config file
 var db = mongoose.connect(config.mongoose.uri);
 autoIncrement.initialize(db);
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-app.set('secret', config.secret); // secret variable
-app.set('tokenlife', config.security.tokenlife);
+app.set('superSecret', config.secret); // secret variable
+app.set('tokenLife', config.security.tokenlife);
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
