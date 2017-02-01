20a53d22b2b6c67789ccd296b973753c02d4ab78
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-01 15:40:19 +0000

    master

--- controllers/user.js
+++ controllers/user.js
@@ -35,7 +35,7 @@ exports.Auth = function(req, res){
   var username = req.body.username;
 
   User.find({"username": username}, function(err, user){
-    return res.send(user.constructor.name);
+    return res.send(user.checkPassword(""));
     if (!user){
       res.json({ success: false, message: 'Authentication failed. User not found. ' + username});
     }else if (user){
