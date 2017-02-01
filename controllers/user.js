0021c04e64f5e7549578140925780c911078c43c
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-01 15:22:05 +0000

    master

--- controllers/user.js
+++ controllers/user.js
@@ -27,7 +27,7 @@ exports.listAll = function(req, res){
 
 exports.listOne = function(req, res){
   User.find({"username":req.params.username}, function(req, result){
-    return res.send(result);
+    return res.send(result instanceof User);
   });
 };
 
