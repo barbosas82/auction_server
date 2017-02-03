35baf8346426aedd05b5b8d370b2a4c7ffccd5f2
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-03 15:37:56 +0000

    .

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ module.exports = function(app){
 
    // route middleware to verify a token
     apiRoutes.use(function(req, res, next) {
-       res.setHeader('Access-Control-Allow-Origin', '*')
+      // res.setHeader('Access-Control-Allow-Origin', '*')
       // check header or url parameters or post parameters for token
       var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
