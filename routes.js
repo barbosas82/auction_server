fa90244103d3d0e0eb54396c0d760fb9043182a3
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-06 09:40:51 +0000

    .

--- routes.js
+++ routes.js
@@ -58,7 +58,7 @@ module.exports = function(app){
       }
     });
 
-    apiRoutes.use('/private', express.static('./private_html'));
+    //apiRoutes.use('/private', express.static('./private_html'));
 
 
     /**********************************************
