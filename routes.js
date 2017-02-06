b1b10e6249a639feca288c342ecc67fb87a3d546
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-06 09:41:23 +0000

    .

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ module.exports = function(app){
   // });
 
   app.use(express.static('public_html'));
-  apiRoutes.use('/private', express.static('./private_html'));
+  app.use('/private', express.static('./private_html'));
 
   apiRoutes.get('/', function(req, res) {
        res.json({ message: 'Welcome to the coolest API on earth!' });
