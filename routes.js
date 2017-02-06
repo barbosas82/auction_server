62b8706f8926c8d31892dbc719518dc7517a315b
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-06 10:14:50 +0000

    .

--- routes.js
+++ routes.js
@@ -10,6 +10,35 @@ module.exports = function(app){
 
   var config = require('./config'); // get our config file
 
+  function validateUser(req, res, next) {
+     res.setHeader('Access-Control-Allow-Origin', '*')
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('secret'), function(err, decoded) {
+        if (err) {
+          //console.log(decoded);
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          //console.log(decoded);
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+          success: false,
+          message: 'No token provided.'
+      });
+    }
+  };
+
   /**********************************************
   ******           DEFAULT MESSAGES         *****
   **********************************************/
@@ -33,34 +62,7 @@ module.exports = function(app){
    app.post('/authenticate', users.Auth); //Get New Token
 
    // route middleware to verify a token
-    apiRoutes.use(function(req, res, next) {
-       res.setHeader('Access-Control-Allow-Origin', '*')
-      // check header or url parameters or post parameters for token
-      var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-      // decode token
-      if (token) {
-        // verifies secret and checks exp
-        jwt.verify(token, app.get('secret'), function(err, decoded) {
-          if (err) {
-            //console.log(decoded);
-            return res.json({ success: false, message: 'Failed to authenticate token.' });
-          } else {
-            // if everything is good, save to request for use in other routes
-            //console.log(decoded);
-            req.decoded = decoded;
-            next();
-          }
-        });
-      } else {
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-      }
-    });
+    apiRoutes.use(validateUser(req, res, next));
 
     //apiRoutes.use('/private', express.static('./private_html'));
 
