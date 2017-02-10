5a70887cd2e5359648d372e2733d1cdf13ba053f
barbosas82 <pedrobarbosa@pb.com.pt>
2017-02-10 15:31:41 +0000

    .

--- private_html/script/custom.js
+++ private_html/script/custom.js
@@ -252,13 +252,13 @@ function populateWantlistTable(field, asc){
       for (var i = 0; i<headertitles.length; i++){
         var th = document.createElement('th');
         var label = document.createTextNode(headertitles[i]);
-        var labelJson = headertitlesJson[i].toString();
+        var labelJson = headertitlesJson[i]. + "";
         var a     = document.createElement('a');
         var img   = document.createElement('img');
         img.src  = 'images/up.png'
         img.width = 12;
         img.height = 12;
-        a.onclick = function() {populateWantlistTable(labelJson, true)};
+        a.onclick = function() { populateWantlistTable(labelJson, true); };
         a.appendChild(img);
 
         var a1     = document.createElement('a');
@@ -266,7 +266,7 @@ function populateWantlistTable(field, asc){
         img1.src  = 'images/down.png';
         img1.width = 12;
         img1.height = 12;
-        a1.onclick = function() {populateWantlistTable(labelJson, false)};
+        a1.onclick = function() { populateWantlistTable(labelJson, false); };
         a1.appendChild(img1);
 
         th.appendChild(label);
